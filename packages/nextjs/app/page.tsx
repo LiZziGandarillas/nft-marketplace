bf48ba7b9e5d58e5725eb6cae06e8028f2247abd
LiZziGandarillas <lizigandarillas@gmail.com>
2024-08-01 02:03:36 -0400

    last changes

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
 import Carousel from "~~/components/Carousel";
 import ClearImage from "~~/components/cards/ClearImage";
 
@@ -13,12 +10,11 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <Carousel/>
-      <div className="grid grid-cols-4 gap-4 p-5">
-          <ClearImage tokenId={0} address={connectedAddress}/>
-          <ClearImage tokenId={1} address={connectedAddress}/>
+      <Carousel />
+      <div className="flex flex-wrap gap-4 p-5">
+        <ClearImage tokenId={0} address={connectedAddress} />
+        <ClearImage tokenId={1} address={connectedAddress} />
       </div>
-      
     </>
   );
 };
